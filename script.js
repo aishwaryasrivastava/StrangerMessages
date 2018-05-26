e51ce80c604c96382092e82549ae35af5faf7c90
Aishwarya Srivastava <aishwaryasrivastava@mail.com>
2018-05-26 15:35:44 -0400

    Added script and stylesheet

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function myFunction() {
     var msg = x.elements[0].value + "<br>";
 	
 }
+
+function createJoyceWall(){
+	var txt = " "
+	for (i = 0; i < 26; i++) {
+		txt = txt + String.fromCharCode(65 + i) + " ";
+		if (i == 7 || i == 16){
+			txt = txt + "<br><br>"
+		}
+}
+	document.getElementById("joyce-wall").innerHTML = txt;
+
+}

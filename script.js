84a394788a6bc950e2738df08f67008e7eb415b4
Aishwarya Srivastava <aishwaryasrivastava@mail.com>
2018-05-28 09:40:02 -0400

    working prototype oml ;-;

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 function myFunction() {
-    var x = document.getElementById("message");
-    var msg = x.elements[0].value + "<br>";
+    var search = window.location.search;
+    var msg = search.substring(search.indexOf("=")+1);
+    createJoyceWall();
+    showMessage(msg);
 	
 }
 
+// Creates the wall with the alphabets and bulbs
 function createJoyceWall(){
 	var txt = " "
+	var wall = document.getElementById("wall");
 	for (i = 0; i < 26; i++) {
-		txt = txt + String.fromCharCode(65 + i) + " ";
-		if (i == 7 || i == 16){
-			txt = txt + "<br><br>"
+		// Get the alphabet
+		var char = String.fromCharCode(65 + i)
+
+		// Create a bulb
+		var bulb = document.createElement("span");
+		bulb.setAttribute("class","dot");
+		bulb.setAttribute("id","bulb_"+char);
+		wall.appendChild(bulb);
+
+		// Create the alphabet
+		var alphabet = document.createElement("span");
+		alphabet.innerHTML = char;
+		alphabet.setAttribute("class", "alphabet");
+		alphabet.setAttribute("id",char);
+		wall.appendChild(alphabet);
+
+		// Line break after H and Q
+		if (char == "H" || char == "Q"){
+			var linebreak = document.createElement("br");
+			wall.appendChild(linebreak);
 		}
+	}
 }
-	document.getElementById("joyce-wall").innerHTML = txt;
+
+// Lights up the bulb object for 0.5s, lights back down
+function lightUp(bulb){
+	var originalColor = bulb.style.backgroundColor;
+	bulb.style.backgroundColor = "white"
+	setTimeout(function(){bulb.style.backgroundColor = originalColor;},500);
+		
+}
+
+// Lights up the letters of message one by one
+function showMessage(message){
+	var i = 0;
+	function show(){
+		var char = message.charAt(i).toUpperCase();
+		var bulb = document.getElementById("bulb_"+char);
+		lightUp(bulb);
+		if(++i < message.length){
+			setTimeout(show,1000);
+		}
+	}
+	setTimeout(show,1000);
 
 }
+
+
+
+
